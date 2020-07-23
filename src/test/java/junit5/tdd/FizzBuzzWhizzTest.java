25886ae4c135bb818363f049b9e132969d6af9be
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-23 11:45:51 +0800

    [FizzBuzzWhizz] [mandy chen] feature : add should_return_fizz_when_play_fizz_buzz_whizz_given_3 test

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class FizzBuzzWhizzTest {
 
     @Test
-    void should_return_1_when_play_fizz_buzz_give_1() {
+    void should_return_1_when_play_fizz_buzz_whizz_given_1() {
         //given
         int number=1;
         //when
@@ -16,5 +16,16 @@ public class FizzBuzzWhizzTest {
         String actual=fizzBuzzWhizz.play(number);
         //then
         assertThat(actual, is("1"));
+    }
+
+    @Test
+    void should_return_fizz_when_play_fizz_buzz_whizz_given_3() {
+        //given
+        int number=3;
+        //when
+        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
+        String actual=fizzBuzzWhizz.play(number);
+        //then
+        assertThat(actual,is("fizz"));
     }
 }
