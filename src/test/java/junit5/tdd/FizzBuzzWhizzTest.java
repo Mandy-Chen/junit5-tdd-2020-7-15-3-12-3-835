755137fff8d533d639ddc1260fa6110af738d5a5
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-23 13:05:23 +0800

    [FizzBuzzWhizz][mandy chen] feature :Add should_return_buzzwhizz_play_fizz_buzz_whizz_given_105 test and implement

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -10,10 +10,10 @@ public class FizzBuzzWhizzTest {
     @Test
     void should_return_1_when_play_fizz_buzz_whizz_given_1() {
         //given
-        int number=1;
+        int number = 1;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
         assertThat(actual, is("1"));
     }
@@ -21,77 +21,78 @@ public class FizzBuzzWhizzTest {
     @Test
     void should_return_fizz_when_play_fizz_buzz_whizz_given_3() {
         //given
-        int number=3;
+        int number = 3;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("fizz"));
+        assertThat(actual, is("fizz"));
     }
 
     @Test
     void should_return_buzz_when_play_fizz_buzz_whizz_given_5() {
         //given
-        int number=5;
+        int number = 5;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("buzz"));
+        assertThat(actual, is("buzz"));
     }
 
     @Test
     void should_return_whizz_when_play_fizz_buzz_whizz_given_7() {
         //given
-        int number=7;
+        int number = 7;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("whizz"));
+        assertThat(actual, is("whizz"));
     }
 
     @Test
     void should_return_fizzbuzz_play_fizz_buzz_whizz_given_15() {
         //given
-        int number=15;
+        int number = 15;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("fizzbuzz"));
+        assertThat(actual, is("fizzbuzz"));
     }
 
     @Test
     void should_return_fizzwhizz_play_fizz_buzz_whizz_given_21() {
         //given
-        int number=21;
+        int number = 21;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("fizzwhizz"));
+        assertThat(actual, is("fizzwhizz"));
     }
 
     @Test
     void should_return_buzzwhizz_play_fizz_buzz_whizz_given_35() {
         //given
-        int number=35;
+        int number = 35;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("buzzwhizz"));
+        assertThat(actual, is("buzzwhizz"));
     }
 
     @Test
     void should_return_fizzbuzzwhiz_play_fizz_buzz_whizz_given_105() {
         //given
-        int number=105;
+        int number = 105;
         //when
-        FizzBuzzWhizz fizzBuzzWhizz=new FizzBuzzWhizz();
-        String actual=fizzBuzzWhizz.play(number);
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+        String actual = fizzBuzzWhizz.play(number);
         //then
-        assertThat(actual,is("fizzbuzzwhizz"));
+        assertThat(actual, is("fizzbuzzwhizz"));
+
     }
 }
