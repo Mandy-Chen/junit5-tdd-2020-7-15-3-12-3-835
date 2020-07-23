b48e3b9e7705c061aa35ac337fb3160dbca9bd6c
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-23 11:41:38 +0800

    [FizzBuzzWhizz][mandy chen] feature : implement given 1 when FizzBuzzWhizz  then 1

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,6 +2,7 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     public String play(int number) {
-        return null;
+
+        return String.valueOf(number);
     }
 }
--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class FizzBuzzWhizzTest {
 
     @Test
-    void should_return_1_when_play_fizz_buzz_given() {
+    void should_return_1_when_play_fizz_buzz_give_1() {
         //given
         int number=1;
         //when
